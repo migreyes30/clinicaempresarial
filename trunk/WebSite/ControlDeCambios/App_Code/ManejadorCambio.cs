b3b9a973053e483462b5370c7eb3add975075b9a
wikiadrian <wikiadrian@434475ea-b5c8-11de-bcc3-8f7829b4396e>
2009-11-14 05:21:09 +0000

    anadir ManejadorCambio.cs

---
+++ trunk/WebSite/ControlDeCambios/App_Code/ManejadorCambio.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Web;
+using System.Data.SqlClient;
+
+/// <summary>
+/// Summary description for ManejadorCambio
+/// </summary>
+public class ManejadorCambio
+{
+    SqlConnection thisConnection;
+    SqlCommand thisCommand;
+
+	public ManejadorCambio()
+	{
+        thisConnection = new SqlConnection(@"Network Library=DBMSSOCN;Data Source=localhost,2798;database=ControlCambios;User id=sa;Password=oracle;");
+        thisConnection.Open();
+
+	}
+
+    public void pasarNCero(int folio , String nombreCambio, String tipoCambio, String areaID, String fechaRecepcion, String fechaAprobacion, String fechaRealizacion, String fileUpload1)
+    {
+        SqlCommand insertando = new SqlCommand("INSERT INTO CAMBIO  (CAMBIO_ID, NOMBRE_CAMBIO, TIPO_CAMBIO, AREA_ID, FECHA_RECEPCION, FECHA_APROBACION, FECHA_REALIZACION, ESTADO_CAMBIO, ARCHIVO)" +
+            "VALUES ('" + (folio) + "', '" + nombreCambio + "', '" + tipoCambio + "', '" + areaID + "', '" + fechaRecepcion + "', '" + fechaAprobacion + "', '" + fechaRealizacion + "', 'Pendiente', '" + fileUpload1 + "')", thisConnection);
+        insertando.ExecuteNonQuery();
+
+        SqlCommand insertadoNCero = new SqlCommand("INSERT INTO NIVEL0 (CAMBIO_ID, STATUS, AREA_ID, FECHA_ASIGNACION)" +
+            " VALUES ('" + (folio) + "', 'Pendiente', '" + areaID + "', '" + fechaRecepcion + "')", thisConnection);
+        insertadoNCero.ExecuteNonQuery();
+
+
+
+
+    }
+
+
+}
